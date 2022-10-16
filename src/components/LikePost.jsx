419a0d54928b7577a3fc59c301dd28b35f7099f5
Aizhamal <aizhamal.batyrbekova@alatoo.edu.kg>
2022-10-17 00:18:31 +0600

    react project

---
+++ src/components/LikePost.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const LikePost = () => {
+  const [like, setLike] = useState(false);
+
+  let style = {
+    float: "right",
+  };
+
+  return (
+    <span style={style} onClick={() => setLike(!like)}>
+      {like ? "❤️" : "🤍"}
+    </span>
+  );
+};
+
+export default LikePost;
